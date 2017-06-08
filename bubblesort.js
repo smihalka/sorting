4e2a154adf2821ee2021a73de4d881350014273e
Shayne Mihalka <shayne@mihalka.com>
2017-06-08 09:44:33 -0700

    bubble sort

--- bubblesort.js
+++ bubblesort.js
@@ -1,3 +1,24 @@
 function bubbleSort(array){
-  return  array;
+
+  for(var i =0;i<array.length;i++){
+    var element = array[i];
+    var nextElm = array[i+1];
+    if(element > nextElm){
+      array[i+1] = element;
+      array[i] = nextElm;
+    }
+  }
+  var bool = false;
+  for(var j =0;j<array.length;j++){
+    var element = array[j];
+    var nextElm = array[j+1];
+    if(element > nextElm){
+      return bubbleSort(array);
+    }
+  }
+  bool = true;
+  if(bool === true){
+      return  array;
+  }
+
 }
--- bubblesort.spec.js
+++ bubblesort.spec.js
@@ -1,6 +1,7 @@
 describe('Bubble Sort', function(){
   it('handles an empty array', function(){
     expect( bubbleSort([]) ).toEqual( [] );
-    expect( bubbleSort([3,4,12,5,11,59,24]) ).toEqual( [3,4,5,11,24,59] );
+    expect( bubbleSort([3,4,12,5,11,59,24]) ).toEqual( [3,4,5,11,12,24,59] );
+    expect( bubbleSort([80,8,21,63,3,4,12,5,11,59,24,1]) ).toEqual( [1,3,4,5,8,11,12,21,24,59,63,80] );
   });
 });
